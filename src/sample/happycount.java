945a98f5ecbb2345a0731611180ed156bd28dc31
zhao-xinyu <69945778+zhao-xinyu@users.noreply.github.com>
2021-07-31 17:20:51 -0700

    trying to figure this out

--- src/sample/happycount.java
+++ src/sample/happycount.java
@@ -1,12 +1,18 @@
 package sample;
 
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.ProgressIndicator;
+
 public class happycount {
 
 	private int happyness = 100; 
+	ProgressBar pb = new ProgressBar(happyness);
+	ProgressIndicator pi = new ProgressIndicator(happyness);
 	
 	public void happyDec(boolean a) {
-		if(!a) {
+		if(!a&& happyness!=0) {
 			happyness -= 10;
+			pb.setProgress(happyness);
 		}
 	}
 }
