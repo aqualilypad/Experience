af697328da149969d528f819672a1be6b53e5909
zhao-xinyu <69945778+zhao-xinyu@users.noreply.github.com>
2021-07-31 17:33:48 -0700

    eh

--- src/sample/happycount.java
+++ src/sample/happycount.java
@@ -4,12 +4,12 @@ import javafx.scene.control.ProgressBar;
 
 public class happycount {
 
-	private double happyness = 100; 
+	private double happyness = 1; 
 	ProgressBar pb = new ProgressBar(happyness);
 
 	public void happyDec(boolean a) {
 		if(!a&& happyness!=0) {
-			happyness -= 10;
+			happyness -= 0.1;
 			pb.setProgress(happyness);
 		}
 	}
