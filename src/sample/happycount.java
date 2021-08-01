7a4193657209a1e355b3e0c189a64fa2caaaa579
zhao-xinyu <69945778+zhao-xinyu@users.noreply.github.com>
2021-07-31 17:27:41 -0700

    this is not working outttt

--- src/sample/happycount.java
+++ src/sample/happycount.java
@@ -1,14 +1,12 @@
 package sample;
 
 import javafx.scene.control.ProgressBar;
-import javafx.scene.control.ProgressIndicator;
 
 public class happycount {
 
-	private int happyness = 100; 
+	private double happyness = 100; 
 	ProgressBar pb = new ProgressBar(happyness);
-	ProgressIndicator pi = new ProgressIndicator(happyness);
-	
+
 	public void happyDec(boolean a) {
 		if(!a&& happyness!=0) {
 			happyness -= 10;
